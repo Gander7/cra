6ba79c5249e60a73711321b89441503b151ecdff
Gander7 <zachjallen@gmail.com>
2020-08-25 13:56:33 -0300

    fix3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 3
+          Learn React Heroku
         </a>
       </header>
     </div>
